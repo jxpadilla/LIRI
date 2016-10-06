f23d31a2637cd5783dbe11c84356716113bd23f9
Juan Padilla <jxpadilla@yahoo.com>
2016-10-06 10:07:38 -0500

    PassedTwitterTest

--- bot.js
+++ bot.js
@@ -1,6 +1,14 @@
-console.log("The bot is starting.")
+// console.log("The bot is starting.");
 
 var Twitter = require('twitter');
 var config = require('./config');
-console.log(config);
+// console.log(config);
 var T = new Twitter(config);
+
+	T.get('search/tweets', {q: '@jxpaustex', count: '20'}, function(err, tweets, response) {
+		if (err) {
+			return console.log(err);
+		} else {
+			console.log("success!");
+		}
+	});
