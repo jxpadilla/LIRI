f73e77d875e5433c5916ee684b4c0ca14a512c29
Juan Padilla <jxpadilla@yahoo.com>
2016-10-08 18:13:10 -0500

    file clean-up

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	consumer_key: 'DpZJaLOjYXjU4i7HyeReAVATS',
-    consumer_secret: '2FXIaWLZVPFRGZsbiRwAUjmsswnRjPKc5iHO65ntxxA4GTQGAg',
-    access_token_key: '783343334177722369-f4Sy7ua1HtXI7iIqGPIozPTTyIImdd6',
-    access_token_secret: 'hwbgNVXG0WwIOAvdA7ivcsSbFSRqUrB61xE1WzJaCOZPe',
-
-};
